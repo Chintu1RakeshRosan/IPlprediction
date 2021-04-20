139f264fa704e5698c557f29a82df3dc6c8727fe
Rakesh Rosan Paital <52162763+Chintu1RakeshRosan@users.noreply.github.com>
2021-04-20 11:28:27 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,116 @@
+# Importing essential libraries
+from flask import Flask, render_template, request
+import pickle
+import numpy as np
+import smtplib
+
+server = "smtp.gmail.com"
+port = 587
+
+sender = "tech2yourself@gmail.com"
+password = "Rakesh@Rosan123"
+
+# Load the Random Forest CLassifier model
+filename = 'first-innings-score-lr-model.pkl'
+regressor = pickle.load(open(filename, 'rb'))
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+	return render_template('index.html')
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    temp_array = list()
+    
+    if request.method == 'POST':
+        
+        batting_team = request.form['batting-team']
+        if batting_team == 'Chennai Super Kings':
+            temp_array = temp_array + [1,0,0,0,0,0,0,0]
+        elif batting_team == 'Delhi Daredevils':
+            temp_array = temp_array + [0,1,0,0,0,0,0,0]
+        elif batting_team == 'Kings XI Punjab':
+            temp_array = temp_array + [0,0,1,0,0,0,0,0]
+        elif batting_team == 'Kolkata Knight Riders':
+            temp_array = temp_array + [0,0,0,1,0,0,0,0]
+        elif batting_team == 'Mumbai Indians':
+            temp_array = temp_array + [0,0,0,0,1,0,0,0]
+        elif batting_team == 'Rajasthan Royals':
+            temp_array = temp_array + [0,0,0,0,0,1,0,0]
+        elif batting_team == 'Royal Challengers Bangalore':
+            temp_array = temp_array + [0,0,0,0,0,0,1,0]
+        elif batting_team == 'Sunrisers Hyderabad':
+            temp_array = temp_array + [0,0,0,0,0,0,0,1]
+            
+            
+        bowling_team = request.form['bowling-team']
+        if bowling_team == 'Chennai Super Kings':
+            temp_array = temp_array + [1,0,0,0,0,0,0,0]
+        elif bowling_team == 'Delhi Daredevils':
+            temp_array = temp_array + [0,1,0,0,0,0,0,0]
+        elif bowling_team == 'Kings XI Punjab':
+            temp_array = temp_array + [0,0,1,0,0,0,0,0]
+        elif bowling_team == 'Kolkata Knight Riders':
+            temp_array = temp_array + [0,0,0,1,0,0,0,0]
+        elif bowling_team == 'Mumbai Indians':
+            temp_array = temp_array + [0,0,0,0,1,0,0,0]
+        elif bowling_team == 'Rajasthan Royals':
+            temp_array = temp_array + [0,0,0,0,0,1,0,0]
+        elif bowling_team == 'Royal Challengers Bangalore':
+            temp_array = temp_array + [0,0,0,0,0,0,1,0]
+        elif bowling_team == 'Sunrisers Hyderabad':
+            temp_array = temp_array + [0,0,0,0,0,0,0,1]
+
+
+
+        overs = float(request.form['overs'])
+        runs = int(request.form['runs'])
+        wickets = int(request.form['wickets'])
+        runs_in_prev_5 = int(request.form['runs_in_prev_5'])
+        wickets_in_prev_5 = int(request.form['wickets_in_prev_5'])
+
+        receiver = request.form['send_mail']
+        
+        temp_array = temp_array + [overs, runs, wickets, runs_in_prev_5, wickets_in_prev_5]
+        
+        data = np.array([temp_array])
+        my_prediction = int(regressor.predict(data)[0])
+
+        subject = "IPL prediction between {} and {}".format(batting_team,bowling_team)
+        message = '''Current batting team is: {}
+After {} over {} scored {} runs loss of {} wicket.
+Based on last 12 year record we predict that the final score will lies between {} to {}'''.format(
+
+                                                                                                          batting_team,
+                                                                                                          overs,
+                                                                                                          batting_team,
+                                                                                                          runs,wickets,
+                                                                                                          my_prediction-10,my_prediction+5)
+
+        body = '''\
+subject: {} 
+
+        
+{}
+
+Thanks&Regards,
+Rakesh'''.format(subject, message)
+        print(batting_team,bowling_team)
+        print(receiver)
+        if len(receiver) > 2:
+            connection = smtplib.SMTP(server, port)
+            connection.starttls()
+
+            connection.login(sender, password)
+            connection.sendmail(sender, receiver, body)
+            connection.quit()
+
+        return render_template('result.html', lower_limit = my_prediction-10, upper_limit = my_prediction+5)
+
+
+
+
+if __name__ == '__main__':
+	app.run(debug=True)
